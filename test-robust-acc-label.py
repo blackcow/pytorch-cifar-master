01f7b5f994dd3becc5a970cc21e313eb22a05de2
Zhonghan <nzhcom123@qq.com>
2021-06-09 16:42:53 +0800

    fair 6.9

--- test-robust-acc-label.py
+++ test-robust-acc-label.py
@@ -145,14 +145,14 @@ def loadmodel_preactresnte(i, factor):
     # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES/e0.031_depth34_widen10_drop0.0/'
     # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES_fair_v1a_T0.1_L1/e0.031_depth34_widen10_drop0.0/'
     # ICML-21
-    ckpt_list = ['trade_10_1.0.pt', 'trade_60_1.0.pt', 'trade_120_1.0.pt']
-    ckpt = '../Robust-Fair/cifar10/models/fair/'
-    net = create_network().cuda()
+    # ckpt_list = ['trade_10_1.0.pt', 'trade_60_1.0.pt', 'trade_120_1.0.pt']
+    # ckpt = '../Robust-Fair/cifar10/models/fair/'
+    # net = create_network().cuda()
     # Fair-AT
-    # ckpt_list = ['model-wideres-epoch75.pt', 'model-wideres-epoch76.pt', 'model-wideres-epoch100.pt']
-    # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES_fair_v1a_T0.1_L1/e0.031_depth34_widen10_drop0.0/'
+    ckpt_list = ['model-wideres-epoch75.pt', 'model-wideres-epoch76.pt', 'model-wideres-epoch100.pt']
+    ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES_fair_v1a_T0.1_L1/e0.031_depth34_widen10_drop0.0/'
     # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES/e0.031_depth34_widen10_drop0.0/'
-    # net = nn.DataParallel(create_network()).cuda()
+    net = nn.DataParallel(create_network()).cuda()
     ckpt += ckpt_list[i]
 
     # print(net)
