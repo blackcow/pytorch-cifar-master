884ef3ae97e589a3bff943a830acfd525324b873
Zhonghan <nzhcom123@qq.com>
2021-06-23 14:44:38 +0800

    fair 6.23

--- test-robust-acc-label.py
+++ test-robust-acc-label.py
@@ -157,9 +157,10 @@ def loadmodel_preactresnte(i, factor):
     # ckpt_list = ['model-wideres-epoch75.pt', 'model-wideres-epoch76.pt', 'model-wideres-epoch100.pt']
     # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES_fair_v1a_T0.1_L1/e0.031_depth34_widen10_drop0.0/'
     # ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES_fair_v1a_T0.1_L1-fl1/e0.031_depth34_widen10_drop0.0/'
-    ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES/e0.031_depth34_widen10_drop0.0/model-wideres-epoch76.pt'
+    ckpt = '../Fair-AT/model-cifar-wideResNet/preactresnet/TRADES/e0.031_depth34_widen10_drop0.0/'
+    ckpt_list = ['model-wideres-epoch75.pt', 'model-wideres-epoch76.pt', 'model-wideres-epoch100.pt']
     net = nn.DataParallel(create_network()).cuda()
-    # ckpt += ckpt_list[i]
+    ckpt += ckpt_list[i]
 
     # print(net)
     net.load_state_dict(torch.load(ckpt))
